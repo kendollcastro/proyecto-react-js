e168bbf6799865c3c766a566eb905f485ddbb535
Kendoll Castro <kendollcastro@gmail.com>
2021-12-08 14:54:31 -0600

    Agrego todos los archivos al proyecto

---
+++ src/hooks/useAlert.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useContext, useState, useEffect, createContext } from "react";
+
+export const AlertContext = createContext()
+
+export const AlertProvider = ({ children }) => {
+    const [message, setMessage] = useState("");
+    const [type, setType] = useState("");
+    const [isOpened, setIsOpened] = useState(false);
+    const [autoClose, setAutoClose] = useState(7000)
+    const [position, setPosition] = useState("right")
+    
+    const sendAlert = ({type, message}) => {
+        setMessage(message);
+        setType(type);
+        setIsOpened(true);
+        setPosition(!position ? 'rigth' : position)
+        setAutoClose(!autoClose ? 5000 : autoClose)
+    }
+
+    useEffect(() => {
+        const alertTimer = setTimeout(() => {
+            setIsOpened(false)
+        }
+            , autoClose)
+        return () => clearTimeout(alertTimer)
+    })
+
+    return (
+        <AlertContext.Provider value={{ sendAlert, type, message, isOpened, position}}>
+            { children }
+        </AlertContext.Provider>
+    )
+}
+
+const useAlert = () => {
+    const context = useContext(AlertContext)
+
+    if (context === undefined) {
+        throw new Error(
+            'You must wrap your application with <AlertProvider /> in order to useAlert().',
+          ) 
+    }
+    return context
+}
+
+export default useAlert
